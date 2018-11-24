d5a2ee5747d71c8b835813eebaec06faec61a6bf
Temitope <tope_olajide@outlook.com>
2018-11-24 21:13:38 +0100

    Update userReducer.js

--- client/src/reducers/userReducer.js
+++ client/src/reducers/userReducer.js
@@ -1,9 +1,13 @@
-import { ADD_USER } from './../actions/type';
+import { ADD_USER,FETCH_USER,DELETE_USER } from './../actions/type';
 
 export default function postReducer(state = [], action) {
   switch (action.type) {
     case ADD_USER:
       return [...state, action.payload];
+      case DELETE_USER:
+      return state.filter(post => post._id !== action.payload.id);
+      case FETCH_USER:
+      return action.posts;
     default:
       return state;
   }
