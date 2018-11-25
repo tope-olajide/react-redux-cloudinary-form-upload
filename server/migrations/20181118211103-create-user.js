4c5beaac4a751856a4872796e3e730e7d67c50aa
Temitope <tope_olajide@outlook.com>
2018-11-25 23:21:26 +0100

    Create 20181118211103-create-user.js

---
+++ server/migrations/20181118211103-create-user.js
@@ -0,0 +1,30 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Users', {
+      id: {
+        allowNull: false,
+        primaryKey: true,
+        type: Sequelize.UUID,
+        defaultValue: Sequelize.UUIDV4
+      },
+      fullname: Sequelize.STRING,
+      username: Sequelize.STRING,
+      email: Sequelize.STRING,
+      description: Sequelize.STRING,
+      imageUrl:Sequelize.STRING,
+      imageId: Sequelize.STRING,
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Users');
+  }
+};
