f26394b4ec782184bef73a6804c17f67ff9d5475
Temitope <tope_olajide@outlook.com>
2018-11-26 01:08:45 +0100

    Update user.js

--- server/controller/user.js
+++ server/controller/user.js
@@ -37,5 +37,126 @@ export default class Users {
                 message: `Error creating user ${error.message}`
             }));
     }
+    modifyUser({ body, params }, res) {
+        const { id } = params;
+        const {
+            fullname,
+            username,
+            email,
+            description,imageUrl,
+            imageId
+        } = body;
+        if (!(fullname || username || email || description)) { 
+            return res.status(401).json({
+                success: true,
+                message: 'please enter all the fields'
+            })
+        }
+        const updateDatabase = ({
+            fullname,
+            username,
+            email,
+            description,
+            imageUrl,
+            imageId
+        }) => {
+          User.findOne({
+            where: { id: id },
+          })
+            .then((foundUser) => {
+              if (!foundUser) {
+                return res.status(404).json({
+                  success: false,
+                  message: 'User not found'
+                });
+              }
+    
+              if (imageId !== foundUser.imageId) {
+                cloudinary.destroy(foundUser.imageId, () => { });
+              }
+    
+              foundUser.updateAttributes({
+                fullname,
+                username,
+                email,
+                description,
+                imageUrl: imageUrl || foundUser.imageUrl,
+                imageId: imageId || foundUser.imageId
+              })
+                .then((user) => {
+                   res.status(200).json({
+                    success: true,
+                    message: 'User record updated',
+                    user: user
+                  });
+                });
+            })
+            .catch((error) => res.status(500).json({
+              success: false,
+              message: 'An error occured' + error
+            }));
+        };
+        updateDatabase({
+            name,
+            username,
+            res,
+            userId,
+            imageUrl,
+            imageId
+          });   
+}
+getUser({ params }, res) {
+    const { userId } = params;
+    User.findOne({
+        where: { id: userId }
+      }) .then(user => res.status(200).json({
+        success: true,
+        message: 'User found',
+        user
+      })).catch((error) => res.status(500).json({
+        success: false,
+        message: 'Error fetching user',
+        error
+      }));
+}
+deleteUser({ params }, res) {
+    const { id } = params;
+    User.findOne({
+        where: {
+          id: id
+        }
+      })  .then((foundUser) => {
+        foundUser.destroy()
+          .then(() => {
+            cloudinary.destroy(foundUser.imageId);
 
+            res.status(200).json({
+              success: true,
+              message: 'User Deleted!'
+            });
+          });
+      })
+      .catch(() => {
+        res.status(400).json({
+          success: false,
+          message: 'Error deleting User'
+        });
+      });
+}
+allUser  (res) {   
+    User.findAll
+      .then((allUsers) => {
+        res.status(200).json({
+            success: true,
+            message: 'User Deleted!',
+            allUsers
+          }) })
+          .catch((error) => {
+            res.status(400).json({
+              success: false,
+              message: 'Error fetching all users User',
+              error
+            });
+          });
+}
 }
