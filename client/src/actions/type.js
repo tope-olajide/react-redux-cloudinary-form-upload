ab37a848942d920a1a1688bd62974050cf577b84
Temitope <tope_olajide@outlook.com>
2018-11-26 01:22:41 +0100

    Update type.js

--- client/src/actions/type.js
+++ client/src/actions/type.js
@@ -1,4 +1,5 @@
 export const ADD_USER = 'ADD_USER';
 export const DELETE_USER = 'DELETE_USER';
-
+export const MODIFY = 'MODIFY';
 export const FETCH_USER = 'FETCH_USER';
+export const FETCH_ALL_USER = 'FETCH_ALL_USER';
