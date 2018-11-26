e0e2bfcb6cef5ad361d42617c8bec1fdf02a81c7
Temitope <tope_olajide@outlook.com>
2018-11-26 21:00:20 +0100

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,5 @@
 import React, { Component } from 'react';
 import CreateProfile from './components/container/CreateProfile'
-import ProfileCard from './components/presentation/ProfileCard'
-import ViewPofile from './components/presentation/ViewProfile'
 class App extends Component {
   render() {
     return (
