0529b3a87c7e4ba16106d4159d72d3f6d966598d
Temitope <tope_olajide@outlook.com>
2018-12-14 02:46:14 +0100

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import { BrowserRouter } from 'react-router-dom';
 import thunk from 'redux-thunk';
 import { Provider } from 'react-redux';
 import rootReducer from './reducers/index';
@@ -9,12 +10,16 @@ import 'bootstrap-css-only/css/bootstrap.min.css';
 import 'mdbreact/dist/css/mdb.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-const store = createStore(rootReducer, applyMiddleware(thunk));
+const store = createStore(rootReducer,compose(
+    applyMiddleware(thunk)
+  ));
 
 store.dispatch(fetchAllUsers());
 
 ReactDOM.render(
     <Provider store={store}>
+    <BrowserRouter>
         <App />
+        </BrowserRouter>
     </Provider>, document.getElementById('root'));
-registerServiceWorker();
+
