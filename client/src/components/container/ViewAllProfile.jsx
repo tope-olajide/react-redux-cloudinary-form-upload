79b4e3759e9c3d021b6afbb65c167817ef4c0cdd
Temitope <tope_olajide@outlook.com>
2018-12-14 02:45:42 +0100

    Update ViewAllProfile.jsx

--- client/src/components/container/ViewAllProfile.jsx
+++ client/src/components/container/ViewAllProfile.jsx
@@ -1,28 +1,44 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
 import ViewAllProfilePage from "./../presentation/ViewAllProfilePage";
-class ViewAllProfile extends Component {
+import EditProfile from "./../container/EditProfile"
+import {fetchSingleUser} from "../../actions/index"
+export class ViewAllProfile extends Component {
+
+  handleViewProfile (userId) {
+    //this.context.router.history.push(`/view-profile/${userId}`);
+    window.location = `/view-profile/${userId}`;
+  }
   render() {
     return (
       <div>
-        <h1>All Posts</h1>
-        {this.props.users.map(user => (
-          <div key={user.id}>
-            <ViewAllProfilePage
-              username={username}
-              fullname={fullname}
-              email={email}
-              imageUrl={imageUrl}
-            />
-            }
-          </div>
-        ))}
+        <h1>All Users</h1>
+        {(!this.props.users.length) ? (
+          <div>No User</div>
+        ) : (
+          this.props.users.map(user => (
+            <div key={user.id}>
+              <ViewAllProfilePage
+                username={user.username}
+                fullname={user.fullname}
+                email={user.email}
+                imageUrl={user.imageUrl}
+                id = {user.id}
+                viewProfile = {this.handleViewProfile}
+              />
+              }
+            </div>
+          ))
+        )}
       </div>
-    );
+    )
   }
 }
 const mapStateToProps = state => {
+  console.log(state.users);
   return {
-    users: state
+    users: state.users
   };
 };
-export default ViewAllProfile;
+
+export default connect(mapStateToProps)(ViewAllProfile);
