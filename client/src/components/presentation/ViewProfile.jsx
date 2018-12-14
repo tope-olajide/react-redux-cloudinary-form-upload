69486c852332cc25174957eebf911fcc5c23222c
Temitope <tope_olajide@outlook.com>
2018-12-14 02:46:09 +0100

    Update ViewProfile.jsx

--- client/src/components/presentation/ViewProfile.jsx
+++ client/src/components/presentation/ViewProfile.jsx
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import "./ProfilePage.css";
 import { Button } from "mdbreact";
 class ViewProfile extends Component {
+
   render() {
     return (
       <div class=" card profile-section mt-5 p-4 col-md-4">
@@ -12,8 +13,11 @@ class ViewProfile extends Component {
         <p class="text-center">{this.props.username}</p>
         <p class="text-center">{this.props.email}</p>
         <p class="user-info">{this.props.description}</p>
-        <Button color="secondary" onClick={() => this.toggle(6)}>
+        <Button color="secondary" onClick={this.props.editProfile}>
           Edit profile
+        </Button>
+        <Button color="secondary" onClick={() => this.toggle(6)}>
+          Delete profile
         </Button>
       </div>
     );
