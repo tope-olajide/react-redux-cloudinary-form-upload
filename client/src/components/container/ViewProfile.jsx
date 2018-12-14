94ba16976e4dfe2f75d00f7cc0bb73fb4dfa2451
Temitope <tope_olajide@outlook.com>
2018-12-14 02:45:48 +0100

    Update ViewProfile.jsx

--- client/src/components/container/ViewProfile.jsx
+++ client/src/components/container/ViewProfile.jsx
@@ -1,30 +1,59 @@
 import React, { Component } from "react";
 import ViewProfile from "./../presentation/ViewProfile";
+import { connect } from "react-redux";
+import {fetchSingleUser} from "../../actions/index"
 class ViewProfileContainer extends Component {
-  render() {
-    if (!posts.length) {
-      return <div>No Users</div>;
+  componentDidMount() {
+    const { id } = this.props.match.params;
+    this.props.dispatch(fetchSingleUser(id));
+    this.setState({
+      userId:id
+    })
+  }
+  constructor() {
+    super();
+    this.state = {
+      userId:''
     }
+    this.handleEditProfile = this.handleEditProfile.bind(this);
+  }
+  handleEditProfile () {
+    window.location = `/edit-profile/${this.state.userId}`;
+    console.log(this.state.userId)
+  }
+  render() {
+    if (!this.props.usersDetails) {
+      return <div>User info not available</div>;
+    } 
     return (
-      <div>
-        {users.map(user => {
-          return (
+      <div>{console.log(this.props.usersDetails.id)}
+      {
             <ViewProfile
-              key={user._id}
-              fullname = {user.fullname}
-              username = {user.username}
-              email = {user.email}
-              description = {user.description}
+              key = {this.props.usersDetails.id}
+              fullname = {this.props.usersDetails.fullname}
+              username = {this.props.usersDetails.username}
+              email = {this.props.usersDetails.email}
+              description = {this.props.usersDetails.description}
+              editProfile = {this.handleEditProfile}
             />
-          );
-        })}
+          
+        }
       </div>
     );
   }
 
-  mapStateToProps = state => {
+/*   mapStateToProps = state => {
     return {
-      users: state.users
+      users: state.users.usersDetails
     };
-  };
+  }; */
 }
+
+const mapStateToProps = state => {
+  console.log(state.users.usersDetails);
+  return {
+    usersDetails: state.users.usersDetails
+  };
+};
+
+export default connect(mapStateToProps)(ViewProfileContainer);
