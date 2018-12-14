48dc1351978fc72079b49ecace4e41df082a3f5d
Temitope <tope_olajide@outlook.com>
2018-12-14 02:45:15 +0100

    Update index.js

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,4 +1,4 @@
-import { ADD_USER,FETCH_USER,DELETE_USER } from '../actions/type';
+import { ADD_USER,FETCH_ALL_USER,DELETE_USER,FETCH_USER,EDIT_USER } from '../actions/type';
 import axios from 'axios';
 
 const apiUrl = 'http://127.0.0.1:5000/api/user';
@@ -24,28 +24,73 @@ export const createUser = ({ fullname, username, email, imageUrl, imageId }) => 
         email: data.email,
         imageUrl: data.imageUrl,
         imageId: data.imageId,
-        editing: false
+        
       }
     }
   };
   export const fetchUsers = (users) => {
+    // console.log(users)
     return {
-      type: FETCH_USER,
+      type: FETCH_ALL_USER,
       users
     }
+    
   };
   
   export const fetchAllUsers = () => {
     return (dispatch) => {
       return axios.get(`${apiUrl}/all-users`)
         .then(response => {
-          dispatch(fetchUsers(response.data))
+          //console.log(response.data)
+          dispatch(fetchUsers(response.data.allUsers))
         })
         .catch(error => {
           throw(error);
         });
     };
   };
+  export const fetchSingleUser = id => {
+    return (dispatch) => {
+      return axios.get(`${apiUrl}/get-user/${id}`)
+      .then(response => {
+        // console.log(response.data.user)
+        dispatch(fetchUser(response.data.user))
+      })
+      .catch(error => {
+        throw(error);
+      });
+    }
+  }
+  export const fetchUser = user => {
+    console.log(user)
+    return {
+      type: FETCH_USER,
+      payload: {
+        user
+      }
+    }
+  }
+  export const editSingleUser = id => {
+    return (dispatch) => {
+      return axios.get(`${apiUrl}/get-user/${id}`)
+      .then(response => {
+        // console.log(response.data.user)
+        dispatch(editUser(response.data.user))
+      })
+      .catch(error => {
+        throw(error);
+      });
+    }
+  }
+  export const editUser = user => {
+    console.log(user)
+    return {
+      type: EDIT_USER,
+      payload: {
+        user
+      }
+    }
+  }
   export const deleteUser = id => {
     return (dispatch) => {
       return axios.get(`${apiUrl}/delete-user/${id}`)
