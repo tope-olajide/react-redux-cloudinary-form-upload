fb9fd74d5a8df5e4503aedbadd558528980fa98c
Temitope <tope_olajide@outlook.com>
2018-12-14 02:45:26 +0100

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,19 @@
-import React, { Component } from 'react';
-import CreateProfile from './components/container/CreateProfile'
-class App extends Component {
-  render() {
-    return (
-      <div>
- <CreateProfile />
-      </div>
-    );
-  }
-}
+import React, {Component} from 'react';
+import { Route, Switch } from 'react-router-dom';
 
-export default App;
+import CreateProfile from './components/container/CreateProfile'
+import EditProfile from './components/container/EditProfile'
+import ViewAllProfile from './components/container/ViewAllProfile'
+import ViewProfile from './components/container/ViewProfile'
+const App = () => (
+  <Switch>
+    <Route exact path="/" component={ViewAllProfile} />
+    <Route exact path="/create-profile" component={CreateProfile} />
+    <Route exact path="/edit-profile/:id" component={EditProfile} />
+    <Route exact path="/view-profile/:id" component={ViewProfile} />
+  </Switch>
+);
+
+export default (
+  App
+); 
